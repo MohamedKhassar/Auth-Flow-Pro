61ecb8f61c5bbb67948740c56c2d854f4d3cdc7b
khassarMohamed <mohamedkhassar775@gmail.com>
2024-02-10 14:23:15 +0100

    begin the front side

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <div className="flex p-5">
+            <h1 className="text-4xl font-bold  ">AuthFlow Pro</h1>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
